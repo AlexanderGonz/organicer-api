ced4abef83c5783f9331d23d98ac6cba5fb91946
Alexander <erickalexander12@gmail.com>
2020-06-18 11:17:03 +0200

    change mongo credentials again. Now new Password and atlas server. And correct database

--- configs/config.js
+++ configs/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // database: process.env.MONGODB || 'mongodb://localhost:27017/proyecto',
-  database: process.env.MONGODB || 'mongodb+srv://alexandergonz:Internet12@organicer-53ui2.mongodb.net/test',
+  database: process.env.MONGODB || 'mongodb+srv://alexandergonz:Internet12@organicer-53ui2.mongodb.net/organicer',
   port: process.env.PORT || 4000,
   secretKey: "miclaveultrasecreta123*"
 }
