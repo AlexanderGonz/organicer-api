b948a8a0ed6ca04cd5b009f1f66debeef951884f
Alexander <erickalexander12@gmail.com>
2020-06-18 14:39:30 +0200

    cambiada la clientUrl

--- users/User.js
+++ users/User.js
@@ -38,7 +38,7 @@ module.exports = (userModel, boardModel, Board, listModel, List, cardModel, Card
               Para empezar a utilizar tu cuenta, deberá hacer click en el siguiente enlace de activación:
             </p>
             <p>
-              <a href="${config.clientUrl}/activate/${doc._id}"><strong>Activar cuenta</strong></a>
+              <a href="${config.clientUrl + '/activate/' + doc._id}" ><strong>Activar cuenta</strong></a>
             </p>
           `
           await Mailer.sendMail(doc.email, 'OrgaNicer - Activación de cuenta', body)
