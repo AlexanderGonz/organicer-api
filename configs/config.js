c0d592b7931376f026fac69f31d9dc35383735db
Alexander <erickalexander12@gmail.com>
2020-06-18 14:52:56 +0200

    cambiada la clientUrl

--- configs/config.js
+++ configs/config.js
@@ -3,5 +3,5 @@ module.exports = {
   database: process.env.MONGODB || 'mongodb+srv://alexandergonz:Internet12@organicer-53ui2.mongodb.net/organicer',
   port: process.env.PORT || 4000,
   secretKey: "miclaveultrasecreta123*",
-  clientUrl: "organicer.herokuapp.com"
+  clientUrl: "http://organicer.herokuapp.com"
 }
